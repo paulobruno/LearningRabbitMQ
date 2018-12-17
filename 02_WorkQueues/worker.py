dd2ecb732aee2a6fac5eceb088b6a39b814db290
Paulo Bruno <paulobruno@alu.ufc.br>
2018-12-17 15:58:54 -0300

    02 - Message acknowledgment

--- 02_WorkQueues/worker.py
+++ 02_WorkQueues/worker.py
@@ -12,13 +12,12 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" %body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_consume(callback,
                       queue='hello',
-                      no_ack=True)
+                      no_ack=False)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 channel.start_consuming()
-
-
