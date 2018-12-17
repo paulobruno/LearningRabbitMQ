229bce1702b89307cfe872590b11f659adfeaf89
Paulo Bruno <paulobruno@alu.ufc.br>
2018-12-17 16:20:09 -0300

    02 - Fair dispatch

--- 02_WorkQueues/worker.py
+++ 02_WorkQueues/worker.py
@@ -4,9 +4,11 @@ import pika
 import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" %body)
@@ -14,10 +16,9 @@ def callback(ch, method, properties, body):
     print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
                       queue='task_queue',
                       no_ack=False)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
 
 channel.start_consuming()
