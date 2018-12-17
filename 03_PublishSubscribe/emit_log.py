68caa870a1702b8d15dee57457cca14164b4cf80
Paulo Bruno <paulobruno@alu.ufc.br>
2018-12-17 16:44:00 -0300

    03 - Exchanges

---
+++ 03_PublishSubscribe/emit_log.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)
+
+print(" [x] Sent %r" % message)
+
+connection.close()
